8a15df89a78adf773ffae0cf56f855372f964bf2
TommyDo <tommy@anvydigital.com>
2021-03-05 17:04:37 -0700

    Edit comments

--- SupportFiles/ububtu-20-04.sh
+++ SupportFiles/ububtu-20-04.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+##
+# Add alias for better command line
+##
 echo $'\n
 alias ll="ls -la"
 alias lash="ls -lash"
@@ -24,24 +27,32 @@ alias tailLog="tail -f /var/log/apache2/access.log" \n
 alias tailAccess="tail -f /var/log/apache2/error.log" \n
 ' >> ~/.bash_profile
 
+###
+# Add Mongo 4.4.3 is default mongo command
+###
 export PATH="/opt/mongodb/4.4.3/bin:$PATH"
 
-source ~/.bash_profile
-
+# Add create mongo data directory and log file
 mkdir -p ~/database/mongodb/4.4.3
 mkdir -p /var/log/mongodb/ && sudo chmod -R 777 /var/log/mongodb
+
+# Copy sample file to Apache default config
 sudo cp -r /html /var/www
+
+# Copy auto add site and auto config ssl
 cp /create-site.sh /var/www
 cp /config-ssl.sh /var/www
 sudo chmod 755 /var/www/create-site.sh
 sudo chmod 755 /var/www/config-ssl.sh
 
+# Config phantomjs
 sudo mv phantomjs-2.1.1-linux-x86_64 /usr/local/share/
 sudo ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
 
-service apache2 start
+# Start services
 /opt/mongodb/4.4.3/bin/mongod --config /opt/mongodb/4.4.3/mongod-4.4.3.conf --dbpath ~/database/mongodb/4.4.3
+service apache2 start
 service php7.2-fpm start
 service php8.0-fpm start
 
-/bin/bash
+/bin/bash source ~/.bash_profile
